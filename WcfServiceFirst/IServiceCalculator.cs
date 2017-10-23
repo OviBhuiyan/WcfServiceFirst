61807c19b8aa05c61eac0e046d095ae387051c53
OVI <OVI@DESKTOP-DF5TG5J>
2017-10-23 23:59:00 +0600

    basic calculator wcf

---
+++ WcfServiceFirst/IServiceCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceFirst
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceCalculator" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceCalculator
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        double AddNumbers(double number1, double number2);
+
+        [OperationContract]
+        double SubstractNumbers(double number1, double number2);
+
+        [OperationContract]
+        double MultiplyNumbers(double number1, double number2);
+        [OperationContract]
+        double DivisionNumber(double number1, double number2);
+    }
+}
---
+++ WcfServiceFirst/ServiceCalculator.svc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceFirst
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceCalculator" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ServiceCalculator.svc or ServiceCalculator.svc.cs at the Solution Explorer and start debugging.
+    public class ServiceCalculator : IServiceCalculator
+    {
+        public double AddNumbers(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        public double DivisionNumber(double number1, double number2)
+        {
+            //throw new NotImplementedException();
+
+            return number1 / number2;
+        }
+
+        public void DoWork()
+        {
+
+        }
+
+        public double MultiplyNumbers(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+
+        public double SubstractNumbers(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+    }
+}
+
+
+//-- reference link
+//http://www.c-sharpcorner.com/UploadFile/dhananjaycoder/four-steps-to-create-first-wcf-service-for-beginners/
